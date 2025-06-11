122cd19934d3d9a34b410cbff88148e81498b6cc
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-06-11 12:23:55 -0500

    Cleanup, copy Python into script

--- interchange-drivers/drivers.py
+++ interchange-drivers/drivers.py
@@ -1,24 +1,3 @@
-# Using the `drivers` module
-
-_Note that the Python snippets of this file are duplicated in `drivers.py` for easier execution_
-
-When using or developing force fields, it can be difficult to know if a force field is being applied correctly - or
-even to define what that means.  Molecular dynamics simulations include inherent randomness (barostats, thermostats,
-etc.) and have many sources of numerical approximation (PME, float rounding, etc.), so it's not tractable to evaluate
-trajectories to validate the application of a force field. Instead, running energy evaluations of systems at the initial
-configuration is a high-value, low-effort way to assess correctness, at least correctness in how force field parameters
-are applied. Different engines ought to report identical or nearly identical values of energies given the same particle
-positions and physics parameters (harmonic force constants, partial charges, etc.). When this is not true, decomposing
-the potential energy function allows a viable starting point for debugging. For more on this approach to validating
-simulation input files, see [Shirts 2017](https://doi.org/10.1007/s10822-016-9977-1).
-
-Interchange has a high-level API for running these energy evaluations. There are several functions for simple checks
-and more options for detailed analyses which have proven useful in developing new features and debugging issues along
-the way.
-
-For starters, let's create an octanol-hexane mixture using Interchange's Packmol wrapper:
-
-```python
 from rich.pretty import pprint
 
 from openff.toolkit import Molecule, Quantity
@@ -39,35 +18,14 @@ topology = pack_box(
 )
 
 print(topology.n_molecules, topology.box_vectors.m_as("angstrom").diagonal())
-# 140 [34.37913464 34.37913464 34.37913464]
-```
 
-We can straightforwardly apply Sage to this topology:
-
-```python
 from openff.toolkit import ForceField
 
 
 interchange = ForceField("openff-2.2.1.offxml").create_interchange(topology)
 interchange
 # Interchange with 7 collections, periodic topology with 3220 atoms.
-```
 
-This `Interchange` object represents the chemical topology - our octanol-hexane mixture - complete with force field
-parameters applied, positions of each atom specified, and the periodic box vectors defined. That's all we need to get
-single-frame energies from MD engines. (Okay, there are some details in between these steps, but Interchange makes some
-reasonable assumptions where necessary.) The `drivers` submodule has functions which make this easy. Each function
-internally follows these steps:
-
-1. Export the `Interchange` object to simulation files/objects needed by a particular MD engine
-2. Call the engine to run a "zero timestep" simulation
-3. Get detailed energy evaluations from the engine
-4. Report energies to the user in a tidy `EnergyReport` object
-
-Here are the energies I get on this machine today - yours might be subtly different for any number of reasons - though
-these may change significantly with different packing results, software updates, etc.:
-
-```python
 from openff.interchange.drivers.gromacs import get_gromacs_energies
 from openff.interchange.drivers.openmm import get_openmm_energies
 
@@ -95,31 +53,25 @@ pprint(openmm_energies)
 # │   │   'Electrostatics': <Quantity(10.1593947, 'kilojoule / mole')>
 # │   }
 # )
-```
 
-Each dictionary represents a portion of the overall potential energy function. Numerically, the valence terms
-agree to 5-6 significant figures. The non-bonded interactions are a little bit more off, mostly because of minor
-differences in how vdW tail corrections are applied and inherent errors in PME calculations. We don't have to squint
-to compare them, however, as there is a handy `EnergyReport.compare` method for this, which raises an error if any terms differ too much:
+try:
+    gromacs_energies.compare(openmm_energies)
+except Exception as error:
+    import ipdb; ipdb.set_trace()
+    print(error)
 
-```python
-gromacs_energies.compare(openmm_energies)
 # ---------------------------------------------------------------------------
 # EnergyError                               Traceback (most recent call last)
-# Cell In[14], line 1
+# Cell In[5], line 1
 # ----> 1 gromacs_energies.compare(openmm_energies)
-#
-# File ~/software/openff-interchange/openff/interchange/drivers/report.py:145, in EnergyReport.compare(self, other, tolerances)
-#     142         errors[key] = diff
-#     144 if errors:
-# --> 145     raise EnergyError(errors)
-#
+# 
+# File ~/software/openff-interchange/openff/interchange/drivers/report.py:146, in EnergyReport.compare(self, other, tolerances)
+#     143         errors[key] = diff
+#     145 if errors:
+# --> 146     raise EnergyError(errors)
+# 
 # EnergyError: {'Bond': <Quantity(0.0017857753, 'kilojoule / mole')>, 'Angle': <Quantity(-0.00267123177, 'kilojoule / mole')>, 'Torsion': <Quantity(-0.00321735958, 'kilojoule / mole')>, 'vdW': <Quantity(0.160138541, 'kilojoule / mole')>, 'Electrostatics': <Quantity(-0.398271038, 'kilojoule / mole')>}
-```
 
-The error lists the energy discrepancies for each term whose difference falls outside a configurable tolerance. We can dig into these discrepancies a little more by passing the `detailed=True` argument to each `get_*_energies` function:
-
-```python
 pprint(get_gromacs_energies(interchange, detailed=True))
 # EnergyReport(
 # │   energies={
@@ -145,21 +97,7 @@ pprint(get_openmm_energies(interchange, combine_nonbonded_forces=False, detailed
 # │   │   'Electrostatics 1-4': <Quantity(-808.356199, 'kilojoule / mole')>
 # │   }
 # )
-```
 
-This gives us an even more detailed view into the parts of the potential energy function. We had already split out the
-vdW and electrostatics interactions from an overall non-bonded energy, but we we can further split out the 1-4 intramolecular
-corrections. This is just a different representation of the same underlying physics - it all adds up to the same
-potential energy in the end - but gives us more information:
-
-1. 1-4 interactions show little difference between OpenMM and GROMACS
-1. There is probably a bug in the detailed representation of vdW energies from GROMACS, as the vdW and vdW 1-4 terms do not add to the total vdW energy displayed above
-1. Differences in non-bonded energies are mostly due to long-range electrostatics
-1. Purely by coincidence, the energy of 1-4 electrostatics of this system happen to almost completely cancel out the rest of the electrostatic interactions - -0.4 kJ/mol looks much alongside 10 kJ/mol than +/- 800 kJ/mol!
-
-So far, we've only been comparing the energy evaluations of two engines. This makes it a little difficult to assess correctness since it's not obvious which one should be used as a reference. Interchange helpfully provides a wrapper around these and more functions which also runs these evaluations with Amber and LAMMPS (if installed):
-
-```python
 from openff.interchange.drivers.all import get_summary_data, get_all_energies
 
 
@@ -168,7 +106,7 @@ pprint(get_all_energies(interchange))
 #   warnings.warn(
 # /Users/mattthompson/software/openff-interchange/openff/interchange/components/mdconfig.py:277: SwitchingFunctionNotImplementedWarning: A switching distance 8.0 angstrom was specified by the force field, but LAMMPS may not implement a switching function as specified by SMIRNOFF. Using a hard cut-off instead. Non-bonded interactions will be affected.
 #   warnings.warn(
-# 
+#
 # {
 # │   'OpenMM': EnergyReport(
 # │   │   energies={
@@ -209,7 +147,7 @@ pprint(get_all_energies(interchange))
 # }
 
 # same thing, just as a table
-get_summary_data(interchange)
+pprint(get_summary_data(interchange))
 # /Users/mattthompson/software/openff-interchange/openff/interchange/components/mdconfig.py:401: SwitchingFunctionNotImplementedWarning: A switching distance 8.0 angstrom was specified by the force field, but Amber does not implement a switching function. Using a hard cut-off instead. Non-bonded interactions will be affected.
 #   warnings.warn(
 # /Users/mattthompson/software/openff-interchange/openff/interchange/components/mdconfig.py:277: SwitchingFunctionNotImplementedWarning: A switching distance 8.0 angstrom was specified by the force field, but LAMMPS may not implement a switching function as specified by SMIRNOFF. Using a hard cut-off instead. Non-bonded interactions will be affected.
@@ -220,44 +158,18 @@ get_summary_data(interchange)
 # Amber    672.308550  3982.404420  4809.756948 -273.205577    39226.493270
 # GROMACS  672.310425  3982.401611  4809.753906 -269.391632        9.761124
 # LAMMPS   672.309552  3982.404216  4809.757186 -659.786776        8.812605
-```
 
-This high-level view makes a few earlier points more obvious:
-
-1. It's (relatively) easy to get valence contributions to match more closely
-1. Electrostatic energies are quite difficult to get matching closely
-1. There's probably something wrong with how electrostatic energies are processed from Amber (but we think the charges are written correctly!)
-1. Different implementations of a vdW switching function / tail correction make have real impacts on energies
-
-So far, we haven't made any modifications to the `Interchange` object since creating it. Why not run an energy minimization and look at the result?
-
-```python
-# this minimization uses OpenMM by default, using other engines might lead to slightly different solutions
 interchange.minimize()
 
-get_summary_data(interchange)
+print(get_summary_data(interchange))
 # /Users/mattthompson/software/openff-interchange/openff/interchange/components/mdconfig.py:401: SwitchingFunctionNotImplementedWarning: A switching distance 8.0 angstrom was specified by the force field, but Amber does not implement a switching function. Using a hard cut-off instead. Non-bonded interactions will be affected.
 #   warnings.warn(
 # /Users/mattthompson/software/openff-interchange/openff/interchange/components/mdconfig.py:277: SwitchingFunctionNotImplementedWarning: A switching distance 8.0 angstrom was specified by the force field, but LAMMPS may not implement a switching function as specified by SMIRNOFF. Using a hard cut-off instead. Non-bonded interactions will be affected.
 #   warnings.warn(
-# 
+#
 #                Bond       Angle      Torsion          vdW  Electrostatics
 # OpenMM   109.458233  334.696692  4287.825448 -4583.424516      -47.190688
 # Amber    109.458042  334.696570  4287.825542 -4586.797027    40220.794510
 # GROMACS  109.458092  334.696960  4287.824707 -4583.264130      -47.506271
 # LAMMPS   109.458097  334.696915  4287.825411 -4973.377790      -48.666073
-```
 
-And there we have it! Energies decreased, and they did so somewhat significantly. Each molecule's internal geometry was
-determined by conformer generation with RDKit or OpenEye and each molecule's position in the simulation box was
-determined by Packmol - each of these methods are good starting points but not quite at an energy minimum, so it's not
-surprising that our energies went down so much.
-
-In this post, we've shown how to use Interchange's `driver` submodule to
-
-1. Get quick high-level energy evaluations from one (or many) simulation engines
-1. Compare energy evaluations across different engines (and their respective exports from Interchange)
-1. Opt in to detailed energy evaluations
-1. Find bugs in the Interchange code itself
-
-What other features might be useful to you? Please [get in touch](https://github.com/openforcefield/openff-interchange/issues) and let us know!
